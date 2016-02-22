c50126030745628b3f6d2cca08e49799d205d8c1
Vladyslav Kazantsev <Specialistvlad@gmail.com>
2016-02-22 13:30:20 +0200

    lab5 6

--- computer-algorithms/src/lab5/main.cpp
+++ computer-algorithms/src/lab5/main.cpp
@@ -60,18 +60,60 @@ Node* createSearchBTree(int *array, int size)
 	return node;
 }
 
+void findRowsSumm(Node *node, int *rows, int currentDepth)
+{
+	if (node == NULL)
+		return;
+
+	rows[currentDepth] += node->data;
+	findRowsSumm(node->left, rows, currentDepth+1);
+	findRowsSumm(node->right, rows, currentDepth+1);
+}
+
+int findRowWithMaxSumm(Node *node, int rowsCount)
+{
+	if (node == NULL)
+		return -1;
+
+	int *rowsSumm = new int[rowsCount];
+	for (int i = 0; i < rowsCount; ++i)
+		rowsSumm[i] = 0;
+
+	findRowsSumm(node, rowsSumm, 0);
+
+	int maxRow = -1;
+	int maxValue = 0;
+	for (int i = 0; i < rowsCount; i++)
+	{
+		if ((rowsSumm[i] >= 0) && (rowsSumm[i] > maxValue)) {
+			maxRow = i;
+			maxValue = rowsSumm[i];
+		}
+	}
+	return maxRow;
+}
+
 int main()
 {
-	int data[] = {25, 50, 55, 0, 10, 5, 30, 40, 35, 15, 45, 60, 75, 70};
+	int data[] = {35, 55, 15, 50, 40, 0, 10, 45, 5, 30, 25};
 	int size = sizeof(data) / (sizeof(data[0]));
 
 	cout << "Exer.1 - Balanced B-Tree" << endl;
 	int *it = (int *)&data;
-	printPretty(createBalancedBTree(NULL, it, size));
+	Node* balancedBTreeRoot = createBalancedBTree(NULL, it, size);
+	printPretty(balancedBTreeRoot);
 	cout << endl;
 
 	cout << "Exer.2 - Search B-Tree" << endl;
-	printPretty(createSearchBTree(data, size));
+	Node* searchBTreeRoot = createSearchBTree(data, size);
+	printPretty(searchBTreeRoot);
+	cout << endl;
+
+	cout << "Exer.6 - Find row with max summ of values from search B-Tree" << endl;
+	printPretty(searchBTreeRoot);
+	cout << "Count starts from 0, -1 = no values found" << endl;
+	cout << "Answer is " << findRowWithMaxSumm(searchBTreeRoot, size) << endl;
+	cout << endl;
 
 	return 0;
 }
